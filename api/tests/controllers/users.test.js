002215f0dc568e2c522882fd3ba0166f67e40630
John Rothera <johnwrothera@gmail.com>
2025-04-01 14:40:12 +0100

    Added password and email validation, adjusted tests and implemented new tests for validation. Adjusted frontend to display messages from the backened, adjusted tests to allow for changes.

--- api/tests/controllers/users.test.js
+++ api/tests/controllers/users.test.js
@@ -11,18 +11,58 @@ describe("/users", () => {
   });
 
   describe("POST, when email and password are provided", () => {
-    test("the response code is 201", async () => {
+    test("the response code is 400 when password is not longer than 8 char", async () => {
       const response = await request(app)
         .post("/users")
         .send({ email: "poppy@email.com", password: "1234" });
 
+      expect(response.statusCode).toBe(400);
+      expect(response.body.message).toEqual(
+        "Password must be at least 8 characters long"
+      );
+    });
+
+    test("the response code is 400 when password has no special char", async () => {
+      const response = await request(app)
+        .post("/users")
+        .send({ email: "poppy@email.com", password: "123456789" });
+
+      expect(response.statusCode).toBe(400);
+      expect(response.body.message).toEqual(
+        "Password must contain at least one special character"
+      );
+    });
+
+    test("the response code is 400 when using invalid email", async () => {
+      const response = await request(app)
+        .post("/users")
+        .send({ email: "johniscool.com", password: "!123456789" });
+
+      expect(response.statusCode).toBe(400);
+      expect(response.body.message).toEqual("Invalid email format");
+    });
+
+    test("the response code is 400 and we get email is required messaage", async () => {
+      const response = await request(app)
+        .post("/users")
+        .send({ email: "", password: "!123456789" });
+
+      expect(response.statusCode).toBe(400);
+      expect(response.body.message).toEqual("Email is required");
+    });
+
+    test("the response code is 201 when password is longer than 8 char and has a special char", async () => {
+      const response = await request(app)
+        .post("/users")
+        .send({ email: "poppy@email.com", password: "12345678!" });
+
       expect(response.statusCode).toBe(201);
     });
 
-    test("a user is created", async () => {
+    test("a user is created with a valid password", async () => {
       await request(app)
         .post("/users")
-        .send({ email: "scarconstt@email.com", password: "1234" });
+        .send({ email: "scarconstt@email.com", password: "123445678!" });
 
       const users = await User.find();
       const newUser = users[users.length - 1];
