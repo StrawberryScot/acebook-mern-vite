f036830c38241756d3a03c8393b24154980cbf66
StrawberryScot <benjaminfloveday@hotmail.co.uk>
2025-04-03 15:14:57 +0100

    uncapitalised 'u' within SignupPage.jsx

--- frontend/src/pages/Signup/SignupPage.jsx
+++ frontend/src/pages/Signup/SignupPage.jsx
@@ -1,4 +1,4 @@
-import { SignupForm } from "../../components/SignUpForm";
+import { SignupForm } from "../../components/SignupForm";
 import { HivemindLogo } from "../../components/HivemindLogo";
 
 export function SignupPage() {
