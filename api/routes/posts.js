e25dab4f76aaa6c52e214c6a5986082dc07abea7
Mohammed Waheed Ahmed <waheedahmed14@gmail.com>
2025-04-03 13:40:22 +0100

    added remove single post functionality

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -7,5 +7,6 @@ const PostsController = require("../controllers/posts");
 router.get("/", PostsController.getAllPosts);
 router.post("/", PostsController.createPost);
 router.put("/:id", PostsController.updatePost);
+router.delete("/:id", PostsController.deletePost);
 
 module.exports = router;
