65cdfb3bdff7bb7f9e4d754fa8428691c8c127a7
John Rothera <johnwrothera@gmail.com>
2025-04-03 16:20:25 +0100

    Created post form that allows us to send a post POST request and updates the database.

--- frontend/src/pages/Feed/FeedPage.jsx
+++ frontend/src/pages/Feed/FeedPage.jsx
@@ -3,6 +3,8 @@ import { useNavigate } from "react-router-dom";
 
 import { getPosts } from "../../services/posts";
 import Post from "../../components/Post";
+import CreatePostForm from "../../components/CreatePostForm";
+import { HivemindLogo } from "../../components/HivemindLogo";
 import LogoutButton from "../../components/LogoutButton";
 
 export function FeedPage() {
@@ -25,16 +27,21 @@ export function FeedPage() {
     }
   }, [navigate]);
 
+  const handlePostCreated = (newPost) => {
+    setPosts([newPost, ...posts]); // Add new post to the top of the feed
+  };
+
   const token = localStorage.getItem("token");
   if (!token) {
     navigate("/login");
-    return;
+    return null;
   }
 
   return (
     <>
-      <h2>Posts</h2>
       <div className="feed" role="feed">
+        <HivemindLogo />
+        <CreatePostForm onPostCreated={handlePostCreated} />
         {posts.map((post) => (
           <Post post={post} key={post._id} />
         ))}
