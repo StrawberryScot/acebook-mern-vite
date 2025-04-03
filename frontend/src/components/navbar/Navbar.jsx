917c34a25e14970188d79c44595bda3d4dae8dba
Iana Bezrodnykh <ianabezrodnykh@hotmail.com>
2025-04-03 12:36:54 +0100

    added conditional rendering for Login button within navbar, still need to implement loggedIn check

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -3,14 +3,19 @@ import { HivemindLogo } from "../../components/HivemindLogo";
 import "./Navbar.css";
 import "../../App.css";
 
-export function Navbar() {
+export function Navbar(props) {
+    const { isLoggedIn } = props;
     return (
         <nav className="navbar">
             <HivemindLogo />
             <div className="auth-links">
-                <Link to="/login" className="button">
-                    Log In
-                </Link>
+                {isLoggedIn ? (
+                    ""
+                ) : (
+                    <Link to="/login" className="button">
+                        Log In
+                    </Link>
+                )}
             </div>
         </nav>
     );
--- frontend/src/pages/Home/HomePage.jsx
+++ frontend/src/pages/Home/HomePage.jsx
@@ -3,9 +3,11 @@ import "./HomePage.css";
 import { Navbar } from "../../components/navbar/Navbar";
 
 export function HomePage() {
+    // TODO: update this when user is logged in
+    const isLoggedIn = false;
     return (
         <div className="home-container">
-            <Navbar />
+            <Navbar isLoggedIn={isLoggedIn} />
             <div className="main-section">
                 <h1>Welcome to HiveMind!</h1>
                 <p>Connect with friends and the world around you</p>
