09e1fac9d5cdef416aec31e8565b14b4aadfb697
Iana Bezrodnykh <ianabezrodnykh@hotmail.com>
2025-04-08 17:56:07 +0100

    Added redux persisting login

--- frontend/src/redux/userSlice.js
+++ frontend/src/redux/userSlice.js
@@ -2,20 +2,22 @@ import { createSlice } from "@reduxjs/toolkit";
 
 // this object defines the initial state for our user slice:
 const initialState = {
-    user: null,
+  user: JSON.parse(localStorage.getItem("user")) || null,
 };
 
 const userSlice = createSlice({
-    name: "user",
-    initialState,
-    reducers: {
-        setUser: (state, action) => {
-            state.user = action.payload; //this will update user state
-        },
-        clearUser: (state) => {
-            state.user = null; //this will reset userr state
-        },
+  name: "user",
+  initialState,
+  reducers: {
+    setUser: (state, action) => {
+      state.user = action.payload; //this will update user state
+      localStorage.setItem("user", JSON.stringify(action.payload));
     },
+    clearUser: (state) => {
+      state.user = null; //this will reset userr state
+      localStorage.removeItem("user");
+    },
+  },
 });
 
 export const { setUser, clearUser } = userSlice.actions; // exporting functions we will use to update state
